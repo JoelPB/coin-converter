ef72273a41ec5482600ff91cf050af7fc15e63e4
JoelPB <coffee.program.joel@gmail.com>
2021-08-26 17:28:42 -0300

    Até a aula ViewModel

--- app/src/main/java/br/com/dio/coinconverter/data/services/AwesomeService.kt
+++ app/src/main/java/br/com/dio/coinconverter/data/services/AwesomeService.kt
@@ -2,10 +2,10 @@ package br.com.dio.coinconverter.data.services
 
 import br.com.dio.coinconverter.data.model.ExchangeResponse
 import retrofit2.http.GET
-import retrofit2.http.Part
+import retrofit2.http.Path
 
 interface AwesomeService {
 
     @GET("/json/last/{coins}")
-    suspend fun exchangeValue(@Part("coins") coins: String): ExchangeResponse
+    suspend fun exchangeValue(@Path("coins") coins: String): ExchangeResponse
 }
---
+++ app/src/main/java/br/com/dio/coinconverter/presentation/MainViewModel.kt
@@ -0,0 +1,48 @@
+package br.com.dio.coinconverter.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.dio.coinconverter.data.model.ExchangeResponseValue
+import br.com.dio.coinconverter.domain.GetExchangeValueUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+
+class MainViewModel(
+    private val getExchangeValueUseCase: GetExchangeValueUseCase
+) : ViewModel() {
+
+    private val _state = MutableLiveData<State>()
+    val state: LiveData<State> = _state
+
+    fun getExchangeValue(coins: String) {
+        viewModelScope.launch {
+            getExchangeValueUseCase(coins)
+                .flowOn(Dispatchers.Main)
+                .onStart {
+                    // usar para mostrar a nossa dialog de progresso
+                    _state.value = State.Loading
+                }
+                .catch {
+                    // usar para mostrar um erro
+                    _state.value = State.Error(it)
+                }
+                .collect {
+                    // usar para mostrar o resultado
+                    _state.value = State.Success(it)
+                }
+        }
+    }
+
+    sealed class State {
+        object Loading: State()
+
+        data class Success(val value: ExchangeResponseValue): State()
+        data class Error(val error: Throwable): State()
+    }
+}
