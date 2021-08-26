4af68317248b5fa62bdc0f10fdd9e5e4f3e8a809
JoelPB <coffee.program.joel@gmail.com>
2021-08-26 15:20:35 -0300

    Até a aula Trabalhando com UseCases

--- app/src/main/java/br/com/dio/coinconverter/data/services/AwesomeService.kt
+++ app/src/main/java/br/com/dio/coinconverter/data/services/AwesomeService.kt
@@ -1,10 +1,11 @@
 package br.com.dio.coinconverter.data.services
 
+import br.com.dio.coinconverter.data.model.ExchangeResponse
 import retrofit2.http.GET
 import retrofit2.http.Part
 
 interface AwesomeService {
 
     @GET("/json/last/{coins}")
-    suspend fun exchangeValues(@Part("coins") coins: String)
+    suspend fun exchangeValue(@Part("coins") coins: String): ExchangeResponse
 }
